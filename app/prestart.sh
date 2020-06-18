32ec3f91b6f55703768d475649ff6a33d93b808e
Sleepingpirates <32914211+Sleepingpirates@users.noreply.github.com>
2020-06-18 02:57:58 -0400

    Update prestart.sh

--- app/prestart.sh
+++ app/prestart.sh
@@ -1,9 +1,7 @@
 #! /usr/bin/env bash
 FILE=/app/app/config/app.db
-if test -f "$FILE";
-then
+if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else
-    python3 setup.py
-    echo "ran setup"
+    python setup.py
 fi
